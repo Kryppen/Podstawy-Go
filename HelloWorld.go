dbe8c8bdf11e73b4fcd9f4034827ec6eb6538ecb
Kryppen <pawel.krynski@ampeksoft.com>
2018-07-27 15:16:14 +0200

    Więcej napisów.

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,7 +1,37 @@
 package main
 
 import "fmt"
+import "errors"
+import "os"
+
+const Pi = 3.1415
 
 func main() {
-	fmt.Printf("Cześć, świecie!")
+
+	jedynka, dwojka, trojka := 1, 2, 3
+
+	fmt.Printf("Cześć, świecie!\n")
+	fmt.Printf("Jedynka to: %v, dwójka to: %v, trójka to: %v.\n", jedynka, dwojka, trojka)
+	fmt.Printf("Liczba Pi to: %v\n", Pi)
+	var i, j int32 = 10, 20
+	var wynik int32 = i + j
+	fmt.Printf("Wynik to: %v\n", wynik)
+
+	napis := "Hello"
+	tablica := []byte(napis)
+	tablica[0] = 'Y'
+	NapisZmieniony := string(tablica)
+	fmt.Printf("Napis zmieniony to: %v", NapisZmieniony)
+	KilkaLinii := `World 
+	i następna linia`
+	napis = napis + KilkaLinii
+	fmt.Printf(napis)
+
+	err := errors.New("emit macho dwarf: elf header corrupted")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Printf("\n\n")
+	fmt.Printf(os.Hostname())
 }
