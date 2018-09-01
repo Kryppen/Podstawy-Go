c306ba09ad432d4319e98bd90550c895610ee02e
Kryppen <pawel.krynski@ampeksoft.com>
2018-09-01 10:32:39 +0200

    OK

--- HelloWorld.go
+++ HelloWorld.go
@@ -8,4 +8,8 @@ func main() {
 
 	fmt.Printf("Cześć, świecie!\n")
 
+	//var tablica = [10]byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
+	//var aPlaster, bPlaster []byte
+
+	//aPlaster = arr
 }
