0194246f504aca007376864abab89f1c355e50ff
Kryppen <p.krynski@ampeksoft.com>
2018-12-17 22:58:51 +0100

    Same package

--- Poligon/Udemy/NinjaLevel13/Ex1/dog/main_test.go
+++ Poligon/Udemy/NinjaLevel13/Ex1/dog/main_test.go
@@ -3,13 +3,11 @@ package dog
 import (
 	"fmt"
 	"testing"
-
-	"github.com/Kryppen/Podstawy-Go/Poligon/Udemy/NinjaLevel13/Ex1/dog"
 )
 
 //TestYears testuje wartość funkcji Years z pakietu dog
 func TestYears(t *testing.T) {
-	x := dog.Years(7)
+	x := Years(7)
 	if x != 49 {
 		t.Error("Powinniśmy mieć 49, a mamy", x)
 	}
@@ -17,7 +15,7 @@ func TestYears(t *testing.T) {
 
 //TestYearsTwo testuje wartość funkcji Years z pakietu dog
 func TestYearsTwo(t *testing.T) {
-	x := dog.Years(7)
+	x := Years(7)
 	if x != 49 {
 		t.Error("Powinniśmy mieć 49, a mamy", x)
 	}
@@ -26,27 +24,27 @@ func TestYearsTwo(t *testing.T) {
 //BenchamrkYears testuje prędkość działania funkcji Years z pakietu dog
 func BenchmarkYears(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		dog.Years(10)
+		Years(10)
 	}
 }
 
 //BenchamrkYears testuje prędkość działania funkcji YearsTwo z pakietu dog
 func BenchmarkYearsTwo(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		dog.YearsTwo(10)
+		YearsTwo(10)
 	}
 }
 
 //ExampleYears pokazuje działanie funkcji Years z pakietu dog.
 func ExampleYears() {
-	fmt.Println(dog.Years(10))
+	fmt.Println(Years(10))
 	//Output:
 	//70
 }
 
 //ExampleYearsTwo pokazuje działanie funkcji YearsTwo z pakietu dog.
 func ExampleYearsTwo() {
-	fmt.Println(dog.YearsTwo(10))
+	fmt.Println(YearsTwo(10))
 	//Output:
 	//70
 }
